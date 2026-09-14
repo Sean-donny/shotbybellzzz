705f4d3ca07a02f4ac37dcd84187b197f2da1944
Sean Donny <deanpatrick007@gmail.com>
2026-09-14 16:02:43 +0100

    Basic responsive footer functionality

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,35 +1,55 @@
+import Image from 'next/image';
+import footerImage from '../favicon.ico';
+
 const Footer = () => {
+  /**
+   * Returns the current year as a number
+   * @returns number
+   */
   const getCurrentYear = () => {
     const d = new Date();
-    const year = d.getFullYear();
-    return year.toString();
+    return d.getFullYear();
+  };
+  /**
+   * Returns the current year as a string
+   * @returns string
+   */
+  const getCurrentYearAsString = () => {
+    return getCurrentYear().toString();
   };
   return (
     <footer className="w-full px-5 py-2 z-40 bg-slate-300">
-      <ul className="flex justify-between flex-col-reverse sm:flex-row">
-        <li className="mt-5 flex justify-center items-end sm:mt-0">
-          <span className="font-line-jp text-gray-600 tracking-tight text-base">
-            &#169;{getCurrentYear()}
-          </span>
+      <ul className="sm:flex justify-between flex-col-reverse sm:flex-row">
+        <li className="mt-5 sm:flex justify-center items-end sm:mt-0 hidden">
+          {/** TODO: Replace with designer's icon */}
+          <Image
+            width={30}
+            height={30}
+            src={footerImage}
+            alt="Logo showing that this website was designed by Sean Donny"
+          />
         </li>
         <li className="flex justify-center items-end">
-          <a
-            className="font-line-jp text-black tracking-tight cursor-pointer text-base opacity-80 hover:opacity-30"
-            href="https://www.youtube.com/@SeanDonny333"
-            target="_blank"
+          <span
+            className="font-line-jp text-gray-600 text-base"
+            title={
+              getCurrentYear()
+                ? `2020 was ${getCurrentYear() - 2020} years ago btw`
+                : ''
+            }
           >
-            YouTube
-          </a>
+            &#169;{getCurrentYearAsString()}
+          </span>
           <a
-            className="font-line-jp text-black tracking-tight cursor-pointer text-base ml-6 sm:ml-9 md:ml-12 lg:ml-4 opacity-80 hover:opacity-30"
-            href="https://instagram.com/sean.donny"
+            className="font-line-jp text-black cursor-pointer text-base ml-6 sm:ml-9 opacity-80 hover:opacity-30"
+            href="https://www.instagram.com/shotbybellzzz/"
             target="_blank"
           >
             Instagram
           </a>
           <a
-            className="font-line-jp text-black tracking-tight cursor-pointer text-base ml-6 sm:ml-9 md:ml-12 lg:ml-4 opacity-80 hover:opacity-30"
-            href="mailto:sean@seandonny.com"
+            className="font-line-jp text-black cursor-pointer text-base ml-6 sm:ml-9 opacity-80 hover:opacity-30"
+            href="mailto:shotbybellzzz@gmail.com"
             target="_blank"
           >
             Contact
