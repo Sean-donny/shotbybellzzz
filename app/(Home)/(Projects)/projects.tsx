646650078c2d885907384d45f5dddb4f3d5ebf7d
Sean Donny <deanpatrick007@gmail.com>
2026-09-23 16:58:14 +0100

    Project album initial scaffolding

--- app/(Home)/(Projects)/projects.tsx
+++ app/(Home)/(Projects)/projects.tsx
@@ -1,7 +1,31 @@
+import { StaticImageData } from 'next/image';
 import React from 'react';
 
 const Projects = () => {
-  return <div className="w-full min-h-screen bg-orange-300 flex">Projects</div>;
+  interface ProjectStyling {
+    backgroundColor: string;
+    headingColor: string;
+    paragraphColor: string;
+    accentColor: string;
+    selectionColor: string;
+  }
+  interface ProjectAlbumProps {
+    projectStyling: ProjectStyling;
+    projectArtwork1: StaticImageData;
+    projectHeading: string;
+    projectDescription: string;
+    projectLink: string;
+  }
+  const ProjectAlbum = ({
+    projectStyling,
+    projectArtwork1,
+    projectHeading,
+    projectDescription,
+    projectLink,
+  }: ProjectAlbumProps) => {
+    return <div id={projectHeading}>{/** Implement project card */}</div>;
+  };
+  return <div className="w-full min-h-screen bg-orange-300 flex">x</div>;
 };
 
 export default Projects;
